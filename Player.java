1fb4c1cecd4599a4fc10e1b2e8ba7857f4f2aec0
guillermotorreon <edgolds71@gmail.com>
2024-09-22 18:29:03 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -24,12 +24,18 @@ public class Personaje {
         // Métodos
         public boolean isAlive() {
             return this.stats.get("health") > 0;
+                /*
+              Es para decir que el personaje player "ESTA VIVO" o si ya se nos fue con san pedro
+            */
         }
 
         public void attack(Enemy opponent) {
             int attackPower = this.stats.get("attackPower");
             opponent.takeDamage(attackPower);
             System.out.println(name + " ataca a " + opponent.getName() + " por " + attackPower + " de daño!");
+                /*
+              para que le de un reverendo golpe al enemigo y se lo mande a san Pedro, o se sigan dando a los batimadrasos
+            */
         }
 
         public void takeDamage(int damage) {
@@ -37,15 +43,26 @@ public class Personaje {
             currentHealth -= damage;
             if (health < 0) health = 0; // Evitar salud negativa
             this.stats.put("health", currentHealth); // Actualizamos el valor en el HashMap
+                /*
+              Aqui es para que el personaje no se nos haga zombie, con la salud negativa, y si es que
+              le parten su mandarina en gajos que se qude bien muerto hasta que el operador del juego decida
+              reiniciar o algo asi
+            */
 
         }
 
         public int getHealth() {
             return this.stats.get("health");
+                 /*
+              Aqui es para que el programa te diga si el personaje sigue vivo, y aver aguanta unas chelas
+            */ 
         }
 
         public void displayInfo() {
             System.out.println("Personaje: " + name + " | Tipo: " + type +  " | Salud: " + this.stats.get("health") + " | Poder de ataque: " + this.stats.get("attackPower") + " | " + description);
+       /*
+              Aqui es para que la maquina te diga la informacion del mismo personaje
+            */
         }
 
     }
