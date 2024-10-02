ddd8da34e5f81119ace3cb0ef6ceecbf3686d317
guillermotorreon <edgolds71@gmail.com>
2024-10-01 18:06:36 -0600

    Delete Rpg directory

--- Rpg/enums/EnemyType.java
+++
@@ -1,6 +0,0 @@
-public enum EnemyType {
-    BASIC,
-    MEDIUM,
-    BOSS,
-    SECRET,
-}
--- Rpg/enums/Stats.java
+++
@@ -1,8 +0,0 @@
-public enum Stats {
-    HP,
-    ATTACK,
-    DEFENSE,
-    SPEED,
-    // Agrega más estadísticas según sea necesario
-}
-
