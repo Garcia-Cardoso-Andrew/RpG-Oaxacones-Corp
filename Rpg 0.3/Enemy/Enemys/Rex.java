9bef297b5cc0cbed19062ebe7b215387a72d9b22
RodrigoVerZa <rodverrob2000@gmail.com>
2024-10-02 10:27:51 -0600

    Rex.java

--- Rpg 0.3/Enemy/Enemys/Rex.java
+++ Rpg 0.3/Enemy/Enemys/Rex.java
@@ -1,3 +1,8 @@
+/*
+   Esta clase representa un enemigo específico en el juego, el Rex.
+   Extiende la clase Enemy y personaliza las estadísticas y el comportamiento de ataque del Rex.
+*/
+
 public class Rex extends Enemy {
     public Rex() {
         super("Rex", 100, 40, 20,"Un dinosaurio rápido y mortal con garras afiladas.");
