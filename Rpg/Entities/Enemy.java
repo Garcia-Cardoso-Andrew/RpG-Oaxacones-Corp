89e7affc6204374a91b8d506a8f42200a22d839c
guillermotorreon <edgolds71@gmail.com>
2024-10-01 18:03:09 -0600

    Update Enemy.java

--- Rpg/Entities/Enemy.java
+++ Rpg/Entities/Enemy.java
@@ -1,125 +1,82 @@
-/* Clase Enemy que representa las estadísticas de los Enemigos */
-/* Mantiene las estadísticas de los enemigos, su ataque y su estado actual */
-public class Enemy {
-    
-    private String name;
-    private int health;
-    private int attackPower;
-    private String type;
-    private String description;
+import javax.swing.*;
 
-    /* Constructor para enemigos */
-    public Enemy(String name, String type) {
+public abstract class Enemy {
+    protected String name;
+    protected int health;
+    protected int attackPower;
+    protected int Defense;
+    protected String description;
+
+    public Enemy(String name, int health, int attackPower,int Defense ,String description) {
         this.name = name;
-        this.type = type;
+        this.health = health;
+        this.attackPower = attackPower;
+        this.description = description;
+        this.Defense = Defense;
+    }
 
-        // Asignar salud y poder de ataque según el tipo de enemigo
-        if (type.equals("Raptor")) {
-            this.health = 50; // Salud del Raptor
-            this.attackPower = 20; // Poder de ataque del Raptor
-            this.description = "Un género de dinosaurios carnívoros bípedos con garras afiladas.";
-        } else if (type.equals("Rex")) {
-            this.health = 100; // Salud del Rex
-            this.attackPower = 40; // Poder de ataque del Rex
-            this.description = "Un dinosaurio carnívoro gigante, conocido por su gran fuerza y tamaño.";
-        } else if (type.equals("Therizino")) {
-            this.health = 80; // Salud del Therizinosaurus
-            this.attackPower = 60; // Poder de ataque del Therizinosaurus
-            this.description = "Un dinosaurio herbívoro con garras enormes, conocido por su extraño aspecto.";
+    public abstract void attack(Player player) ;
+
+
+
+    /* Método para recibir daño */
+    public void takeDamage(int damage) {
+        if (Defense > 0) {
+            // Reducir defensa primero
+            int remainingDamage = damage - Defense;
+            if (remainingDamage < 0) {
+                remainingDamage = 0; // Evitar daño negativo
+            }
+            Defense -= damage;
+            if (Defense < 0) {
+                Defense = 0; // Evitar defensa negativa
+            }
+
+            // Mostrar la reducción de defensa
+            JOptionPane.showMessageDialog(null, name + " ha recibido " + damage + " de daño. Defensa restante: " + Defense);
+
+            // Si queda daño restante después de reducir la defensa, aplicarlo a la salud
+            if (remainingDamage > 0) {
+                applyDamageToHealth(remainingDamage);
+            }
+
         } else {
-            throw new IllegalArgumentException("Tipo de enemigo no válido");
+            // Si no queda defensa, todo el daño va a la salud
+            applyDamageToHealth(damage);
         }
     }
-
-    // Métodos
-    public boolean isAlive() {
-        return health > 0;
-    }
-
-    public void attack(Player player) { // Cambiado para atacar al jugador
-        player.takeDamage(attackPower);
-        System.out.println(name + " ataca a " + player.getName() + " por " + attackPower + " de daño!");
-    }
-
-    public void takeDamage(int damage) {
+    /* Método para aplicar daño a la salud */
+    private void applyDamageToHealth(int damage) {
         health -= damage;
         if (health < 0) {
             health = 0; // Evitar salud negativa
         }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public String getType() { // Método para obtener el tipo de enemigo
-        return type;
+        JOptionPane.showMessageDialog(null, name + " ha recibido " + damage + " de daño. Salud restante: " + health);
     }
 
     public void displayInfo() {
-        System.out.println("Enemigo: " + name + " | Tipo: " + type + " | " + description + " | Salud: " + health + " | Poder de ataque: " + attackPower);
-    }
-}
--------------------
- package rpg.entities.enemies;
-
-import rpg.entities.GameCharacter;
-import rpg.enums.Stats;
-
-public abstract class Enemy extends GameCharacter {
-
-    protected EnemyType type;
-
-    public Enemy(String name) {
-        super(name);
+        System.out.println("Enemigo: " + name + "\nSalud: " + health + "\nPoder de ataque: " + attackPower + "\nDescripción: " + description);
     }
 
-    public abstract void getLoot();
-
-    public abstract void attack(GameCharacter enemy);
-
-    public EnemyType getType() {
-        return type;
-    }
-}
-
-----------------------
- package rpg.entities.enemies;
-
-import rpg.entities.GameCharacter;
-import rpg.enums.EnemyType;
-
-    
-public abstract class Enemy {
-    private String name;
-    private EnemyType type;
-
-    public Enemy(String name) {
-        this.name = name;
+    public boolean isAlive() {
+        return health > 0;
     }
 
-    public abstract void getLoot();
-
-    public abstract void attack(GameCharacter enemy);
-
-    public EnemyType getType() {
-        return type;
-    }
-
-    public void setType(EnemyType type) {
-        this.type = type;
-    }
-
+    // Método getter para el nombre
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    // Método getter para el poder de ataque
+    public int getAttackPower() {
+        return attackPower;
+    }
+    /* Método getter para la defensa */
+    public int getDefense() {
+        return Defense;
+    }
+    /* Método setter para la defensa */
+    public void setDefense(int Defense) {
+        this.Defense = Defense;
     }
 }
-//** esto es lo que me dio la ia y lo cheque en el editor y por si solo no tiene errores que imposibilitarian que corran
