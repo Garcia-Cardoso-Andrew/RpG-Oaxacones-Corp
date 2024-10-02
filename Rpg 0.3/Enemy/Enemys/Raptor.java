3a73e8e6674c53c7e877ad47bb152e228a117b77
RodrigoVerZa <rodverrob2000@gmail.com>
2024-10-02 10:27:12 -0600

    Raptor.java

--- Rpg 0.3/Enemy/Enemys/Raptor.java
+++ Rpg 0.3/Enemy/Enemys/Raptor.java
@@ -1,3 +1,9 @@
+/*
+   Esta clase representa un enemigo específico en el juego, el Raptor.
+   Extiende la clase Enemy y personaliza las estadísticas y el comportamiento de ataque del Raptor.
+*/
+
+
 public class Raptor extends Enemy {
     public Raptor() {
         super("Raptor", 50, 20,5 ,"Un dinosaurio rápido y mortal con garras afiladas."); // Defensa ligera
