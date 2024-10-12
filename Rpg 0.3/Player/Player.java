b9688ebdd242f2db7df6520b4afdd852e8ce2322
guillermotorreon <edgolds71@gmail.com>
2024-10-11 22:52:53 -0600

    todavia me faltan algunas cosas pero hay boi

--- Rpg 0.3/Player/Player.java
+++ Rpg 0.3/Player/Player.java
@@ -101,9 +101,3 @@ public class Player {
     }
 }
 
-/* Enum que define las estadísticas del jugador */
-public enum Stats {
-    ATTACK,  // Poder de ataque
-    HP,       // Salud
-    DEFENSE  // defensa
-}
---
+++ Rpg 0.3/Player/Stats.java
@@ -0,0 +1,8 @@
+package Player;
+
+/* Enum que define las estadísticas del jugador */
+public enum Stats {
+    ATTACK,  // Poder de ataque
+    HP,       // Salud
+    DEFENSE  // defensa
+}
