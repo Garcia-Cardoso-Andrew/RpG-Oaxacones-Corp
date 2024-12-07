18fd6f397ad06780f45aeedd10e7cb81ca40f3aa
RodrigoVerZa <rodverrob2000@gmail.com>
2024-12-06 19:02:12 -0600

    comentado chitin.java

--- Rpg 0.3/items/armaduras/chitin.java
+++ Rpg 0.3/items/armaduras/chitin.java
@@ -1,7 +1,14 @@
 package items.armaduras;
 
-    public class chitin extends Armor {
-        public chitin() {
-            super("armadura de chitin", 43);
-        }
+// La clase 'chitin' representa una armadura específica hecha de quitina (chitin en inglés).
+// Extiende la clase 'Armor', lo que significa que hereda todas las propiedades y métodos de esta clase base.
+public class chitin extends Armor {
+
+    // Constructor de la clase 'chitin'.
+    public chitin() {
+        // Llama al constructor de la clase base 'Armor' con valores específicos:
+        // - El nombre de la armadura se establece como "armadura de chitin".
+        // - El bono de defensa se establece en 43.
+        super("armadura de chitin", 43);
     }
+}
