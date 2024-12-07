4a2858d514090a0c45979b7f88ec8038880418ab
RodrigoVerZa <rodverrob2000@gmail.com>
2024-12-06 18:56:32 -0600

    comentario Fabric.java

--- Rpg 0.3/items/armaduras/Fabric.java
+++ Rpg 0.3/items/armaduras/Fabric.java
@@ -1,6 +1,14 @@
 package items.armaduras;
-public class Fabric  extends Armor {
-        public Fabric () {
-        super("armadura de tela" ,25);
+
+// La clase 'Fabric' representa una armadura específica hecha de tela.
+// Extiende la clase 'Armor', lo que significa que hereda todas las propiedades y métodos de la misma.
+public class Fabric extends Armor {
+
+    // Constructor de la clase 'Fabric'.
+    public Fabric() {
+        // Llama al constructor de la clase base 'Armor' con valores específicos:
+        // - El nombre de la armadura se establece como "armadura de tela".
+        // - El bono de defensa se establece en 25.
+        super("armadura de tela", 25);
     }
 }
